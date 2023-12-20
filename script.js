300db19b5a7ee53efae84e813dc10bb630a62abb
Ayaaa13 <aiahnava5@gmail.com>
2023-12-20 17:21:45 +0800

    Update

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
-    const buttons = document.querySelectorAll("button");
-    const result = document.querySelector(".result")
+const buttons = document.querySelectorAll("button");
+const result = document.querySelector(".result")
+const specialChars = ["%", "-", "+", "/", "*", "="];
+
+let output = "";
+
+function calculate(buttonValue) {
+    if(buttonValue === "=" && output !== ""){
+        output = eval(output.replace("%", "/100"));
+    } else if (buttonValue === "AC") {
+        output = "";
+    } else if (buttonValue === "DEL") {
+        output = output.toString().slice(0, -1);
+    } else {
+       if (output === "" && specialChars.includes(buttonValue)) return;
+       output += buttonValue ; 
+    }
+    result.value = output;
+}
+
 
    buttons.forEach((button) => {
     button.addEventListener("click", function(e){
         e.preventDefault();
-        let value = e.target.dataset.value;
-        result.value += value;
+        let inputValue = e.target.dataset.value;
+        calculate(inputValue)
     })
 })
